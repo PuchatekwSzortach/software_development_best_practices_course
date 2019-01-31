f95d1ed93cf5b0e49d7c1974438b2157da8a6300
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2019-01-31 11:17:42 +0900

    Refactored Warrior class
    Added new __repr__ implementation

--- git_tutorial/advanced.py
+++ git_tutorial/advanced.py
@@ -11,3 +11,8 @@ class Warrior(basic.Person):
 
         super().__init__(name, age)
         self.strength = strength
+
+    def __repr__(self):
+
+        basic_representation = super().__repr__()
+        return basic_representation + ", strength level {}".format(self.strength)
